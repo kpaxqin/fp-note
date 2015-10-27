6816bfd024f4389de89f7924870a6ef1633536e8
Junwen Qin <jwqin@thoughtworks.com>
2015-10-27 22:42:03 +0800

    [step_1]handle event without any functional programming; rewrite with ES6

--- dist/bundle.js
+++ dist/bundle.js
@@ -46,62 +46,95 @@
 
 	'use strict';
 	
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+	
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+	
 	function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 	
-	var React = __webpack_require__(1);
-	var ReactDOM = __webpack_require__(157);
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 	
-	var Form = React.createClass({
-	  displayName: 'Form',
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 	
-	  getInitialState: function getInitialState() {
-	    return {
+	var _react = __webpack_require__(1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactDom = __webpack_require__(157);
+	
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+	
+	var Form = (function (_React$Component) {
+	  _inherits(Form, _React$Component);
+	
+	  function Form(props) {
+	    _classCallCheck(this, Form);
+	
+	    _get(Object.getPrototypeOf(Form.prototype), 'constructor', this).call(this, props);
+	    this.state = {
 	      name: '',
 	      address: ''
 	    };
-	  },
-	  setField: function setField(key) {
-	    var _this = this;
-	
-	    return function (e) {
-	      _this.setState(_defineProperty({}, key, e.target.value));
-	    };
-	  },
-	  render: function render() {
-	    var _state = this.state;
-	    var name = _state.name;
-	    var address = _state.address;
-	
-	    return React.createElement(
-	      'div',
-	      null,
-	      React.createElement(
-	        'form',
-	        null,
-	        React.createElement(
-	          'label',
-	          null,
-	          'name: '
-	        ),
-	        React.createElement('input', {
-	          value: name,
-	          onChange: this.setField('name')
-	        }),
-	        React.createElement(
-	          'label',
-	          null,
-	          'address: '
-	        ),
-	        React.createElement('input', {
-	          value: address,
-	          onChange: this.setField('address')
-	        })
-	      )
-	    );
 	  }
-	});
 	
-	ReactDOM.render(React.createElement(Form, null), document.getElementById('container'));
+	  _createClass(Form, [{
+	    key: 'setField',
+	    value: function setField(key) {
+	      var _this = this;
+	
+	      return function (e) {
+	        _this.setState(_defineProperty({}, key, e.target.value));
+	      };
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var _state = this.state;
+	      var name = _state.name;
+	      var address = _state.address;
+	
+	      return _react2['default'].createElement(
+	        'div',
+	        null,
+	        _react2['default'].createElement(
+	          'h3',
+	          null,
+	          'Demo for my FP note'
+	        ),
+	        _react2['default'].createElement(
+	          'form',
+	          null,
+	          _react2['default'].createElement(
+	            'label',
+	            null,
+	            'name: '
+	          ),
+	          _react2['default'].createElement('input', {
+	            value: name,
+	            onChange: this.setField('name').bind(this)
+	          }),
+	          _react2['default'].createElement(
+	            'label',
+	            null,
+	            'address: '
+	          ),
+	          _react2['default'].createElement('input', {
+	            value: address,
+	            onChange: this.setField('address').bind(this)
+	          })
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return Form;
+	})(_react2['default'].Component);
+	
+	;
+	
+	_reactDom2['default'].render(_react2['default'].createElement(Form, null), document.getElementById('container'));
 
 /***/ },
 /* 1 */
--- src/index.jsx
+++ src/index.jsx
@@ -1,40 +1,42 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var Form = React.createClass( {
-  getInitialState(){
-    return {
+class Form extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
       name: '',
       address: ''
     }
-  },
+  }
   setField(key){
     return (e)=>{
       this.setState({
         [key]: e.target.value
       })
     }
-  },
+  }
   render(){
     const {name, address} = this.state;
     return (
       <div>
+        <h3>Demo for my FP note</h3>
         <form>
           <label >name: </label>
           <input
             value={name}
-            onChange={this.setField('name')}
+            onChange={this.setField('name').bind(this)}
             />
           <label >address: </label>
           <input
             value={address}
-            onChange={this.setField('address')}
+            onChange={this.setField('address').bind(this)}
             />
         </form>
       </div>
 
     )
   }
-});
+}
 
 ReactDOM.render(<Form/>, document.getElementById('container'));
